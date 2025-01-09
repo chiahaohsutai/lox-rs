57f1a63a0c8df98810b7f7c6670be0a25091135b
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2025-01-08 21:44:44 -0500

    updated interpret fn

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,17 @@ mod lexer;
 mod parser;
 mod tokens;
 
+use parser::Declaration;
+
 use crate::interpreter::Evaluate;
 
 pub fn interpret<T: AsRef<str>>(text: T) {
     let tokens = lexer::scan(text.as_ref().to_string());
-    let statements = parser::parse(tokens);
-    match statements {
-        Ok(statements) => {
-            for stmt in statements {
-                let result = stmt.evaluate();
+    let declarations = parser::parse(tokens);
+    match declarations {
+        Ok(declarations) => {
+            for decl in declarations {
+                let result = decl.evaluate();
                 match result {
                     Ok(Some(literal)) => println!("{}", literal),
                     Ok(None) => (),
