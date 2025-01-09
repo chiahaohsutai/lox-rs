b9b70974388c4960d027b50a313e81345177360c
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2025-01-08 21:43:51 -0500

    added initial declaration def

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,10 @@
-use std::{fmt::Display, slice::Iter, iter::Peekable, result::Result};
 use crate::tokens::{self, Keyword, Operator, Punctuation, Token};
+use std::{
+    fmt::{DebugList, Display},
+    iter::Peekable,
+    result::Result,
+    slice::Iter,
+};
 
 #[derive(Debug, PartialEq)]
 pub enum LiteralExpr {
@@ -26,6 +31,7 @@ pub enum Expr {
     UnaryExpr(Operator, Box<Expr>),
     Grouping(Box<Expr>),
     Literal(LiteralExpr),
+    Variable(String),
 }
 
 impl Display for Expr {
@@ -35,15 +41,20 @@ impl Display for Expr {
             Self::UnaryExpr(op, expr) => write!(f, "({} {})", op, expr),
             Self::Grouping(expr) => write!(f, "(group {})", expr),
             Self::Literal(literal) => write!(f, "{}", literal),
+            Self::Variable(name) => write!(f, "(variable {})", name),
         }
     }
 }
 
-fn expression(tokens: Peekable<Iter<Token>>) -> Result<(Expr, Peekable<Iter<Token>>), String> {
-   Ok(equality(tokens)?)
+fn expression(
+    tokens: Peekable<Iter<Token>>,
+) -> Result<(Expr, Peekable<Iter<Token>>), (String, Peekable<Iter<Token>>)> {
+    Ok(equality(tokens)?)
 }
 
-fn equality(tokens: Peekable<Iter<Token>>) -> Result<(Expr, Peekable<Iter<Token>>), String> {
+fn equality(
+    tokens: Peekable<Iter<Token>>,
+) -> Result<(Expr, Peekable<Iter<Token>>), (String, Peekable<Iter<Token>>)> {
     let (mut expr, mut tokens) = comparison(tokens)?;
     while let Some(&Token::OPER(op, _)) = tokens.peek() {
         match op {
@@ -55,11 +66,13 @@ fn equality(tokens: Peekable<Iter<Token>>) -> Result<(Expr, Peekable<Iter<Token>
             }
             _ => break,
         }
-    };
+    }
     Ok((expr, tokens))
 }
 
-fn comparison(tokens: Peekable<Iter<Token>>) -> Result<(Expr, Peekable<Iter<Token>>), String> {
+fn comparison(
+    tokens: Peekable<Iter<Token>>,
+) -> Result<(Expr, Peekable<Iter<Token>>), (String, Peekable<Iter<Token>>)> {
     let (mut expr, mut tokens) = term(tokens)?;
     while let Some(&Token::OPER(op, _)) = tokens.peek() {
         match op {
@@ -71,11 +84,13 @@ fn comparison(tokens: Peekable<Iter<Token>>) -> Result<(Expr, Peekable<Iter<Toke
             }
             _ => break,
         }
-    };
+    }
     Ok((expr, tokens))
 }
 
-fn term(tokens: Peekable<Iter<Token>>) -> Result<(Expr, Peekable<Iter<Token>>), String> {
+fn term(
+    tokens: Peekable<Iter<Token>>,
+) -> Result<(Expr, Peekable<Iter<Token>>), (String, Peekable<Iter<Token>>)> {
     let (mut expr, mut tokens) = factor(tokens)?;
     while let Some(&Token::OPER(op, _)) = tokens.peek() {
         match op {
@@ -87,11 +102,13 @@ fn term(tokens: Peekable<Iter<Token>>) -> Result<(Expr, Peekable<Iter<Token>>), 
             }
             _ => break,
         }
-    };
+    }
     Ok((expr, tokens))
 }
 
-fn factor(tokens: Peekable<Iter<Token>>) -> Result<(Expr, Peekable<Iter<Token>>), String> {
+fn factor(
+    tokens: Peekable<Iter<Token>>,
+) -> Result<(Expr, Peekable<Iter<Token>>), (String, Peekable<Iter<Token>>)> {
     let (mut expr, mut tokens) = unary(tokens)?;
     while let Some(&Token::OPER(op, _)) = tokens.peek() {
         match op {
@@ -103,11 +120,13 @@ fn factor(tokens: Peekable<Iter<Token>>) -> Result<(Expr, Peekable<Iter<Token>>)
             }
             _ => break,
         }
-    };
+    }
     Ok((expr, tokens))
 }
 
-fn unary(tokens: Peekable<Iter<Token>>) -> Result<(Expr, Peekable<Iter<Token>>), String> {
+fn unary(
+    tokens: Peekable<Iter<Token>>,
+) -> Result<(Expr, Peekable<Iter<Token>>), (String, Peekable<Iter<Token>>)> {
     let mut tokens = tokens;
     if let Some(&Token::OPER(op, _)) = tokens.peek() {
         match op {
@@ -123,28 +142,39 @@ fn unary(tokens: Peekable<Iter<Token>>) -> Result<(Expr, Peekable<Iter<Token>>),
     }
 }
 
-fn primary(tokens: Peekable<Iter<Token>>) -> Result<(Expr, Peekable<Iter<Token>>), String> {
+fn primary(
+    tokens: Peekable<Iter<Token>>,
+) -> Result<(Expr, Peekable<Iter<Token>>), (String, Peekable<Iter<Token>>)> {
     let mut tokens = tokens;
     if let Some(token) = tokens.next() {
         match token {
             tokens::Token::NUM(n, _) => Ok((Expr::Literal(LiteralExpr::Number(*n)), tokens)),
-            tokens::Token::STR(s, _) => Ok((Expr::Literal(LiteralExpr::String(String::from(s))), tokens)),
-            tokens::Token::KWD(Keyword::TRUE, _) => Ok((Expr::Literal(LiteralExpr::Boolean(true)), tokens)),
-            tokens::Token::KWD(Keyword::FALSE, _) => Ok((Expr::Literal(LiteralExpr::Boolean(false)), tokens)),
+            tokens::Token::STR(s, _) => {
+                Ok((Expr::Literal(LiteralExpr::String(String::from(s))), tokens))
+            }
+            tokens::Token::KWD(Keyword::TRUE, _) => {
+                Ok((Expr::Literal(LiteralExpr::Boolean(true)), tokens))
+            }
+            tokens::Token::KWD(Keyword::FALSE, _) => {
+                Ok((Expr::Literal(LiteralExpr::Boolean(false)), tokens))
+            }
             tokens::Token::KWD(Keyword::NIL, _) => Ok((Expr::Literal(LiteralExpr::Nil), tokens)),
+            tokens::Token::IDEN(identifier, _) => {
+                Ok((Expr::Variable(String::from(identifier)), tokens))
+            }
             tokens::Token::PUNC(Punctuation::LPAREN, _) => {
                 let (expr, mut tokens) = expression(tokens)?;
                 if let Some(Token::PUNC(Punctuation::RPAREN, _)) = tokens.peek() {
                     let _ = tokens.next();
                     Ok((Expr::Grouping(Box::new(expr)), tokens))
                 } else {
-                    Err(String::from("Expected closing parenthesis"))
+                    Err((String::from("Expected closing parenthesis"), tokens))
                 }
             }
-            _ => Err(format!("Unexpected token {:?}", token)),
+            _ => Err((format!("Unexpected token {:?}", token), tokens)),
         }
     } else {
-        Err(String::from("Unexpected end of input"))
+        Err((String::from("Unexpected end of input"), tokens))
     }
 }
 
@@ -153,51 +183,132 @@ pub enum Stmt {
     Print(Expr),
 }
 
-pub fn parse(tokens: Vec<Token>) -> Result<Vec<Stmt>, String> {
-    let mut statements: Vec<Stmt> = vec![];
-    let mut tokens = tokens.iter().peekable();
-
-    while tokens.peek().is_some() {
-        let token = tokens.peek().unwrap();
-        match token {
-            Token::EOF(_) => break,
-            _ => {
-                let (stmt, tkns) = statement(tokens)?;
-                statements.push(stmt);
-                tokens = tkns;
-            },
-        }
-    };
-    Ok(statements)
-}
-
-fn statement(mut tokens: Peekable<Iter<Token>>) -> Result<(Stmt, Peekable<Iter<Token>>), String> {
+fn statement(
+    mut tokens: Peekable<Iter<Token>>,
+) -> Result<(Stmt, Peekable<Iter<Token>>), (String, Peekable<Iter<Token>>)> {
     match tokens.peek() {
         Some(Token::KWD(Keyword::PRINT, _)) => {
             let _ = tokens.next();
             print_stmt(tokens)
-        }, 
+        }
         Some(_) => expr_stmt(tokens),
-        None => Err(String::from("Unexpected end of input")),
+        None => Err((String::from("Unexpected end of input"), tokens)),
     }
 }
 
-fn print_stmt(tokens: Peekable<Iter<Token>>) -> Result<(Stmt, Peekable<Iter<Token>>), String> {
+fn print_stmt(
+    tokens: Peekable<Iter<Token>>,
+) -> Result<(Stmt, Peekable<Iter<Token>>), (String, Peekable<Iter<Token>>)> {
     let (expr, mut tokens) = expression(tokens)?;
     if let Some(Token::PUNC(Punctuation::SEMICOLON, _)) = tokens.peek() {
         let _ = tokens.next();
         Ok((Stmt::Print(expr), tokens))
     } else {
-        Err(String::from("Expected semicolon"))
+        Err((String::from("Expected semicolon"), tokens))
     }
 }
 
-fn expr_stmt(tokens: Peekable<Iter<Token>>) -> Result<(Stmt, Peekable<Iter<Token>>), String> {
+fn expr_stmt(
+    tokens: Peekable<Iter<Token>>,
+) -> Result<(Stmt, Peekable<Iter<Token>>), (String, Peekable<Iter<Token>>)> {
     let (expr, mut tokens) = expression(tokens)?;
     if let Some(Token::PUNC(Punctuation::SEMICOLON, _)) = tokens.peek() {
         let _ = tokens.next();
         Ok((Stmt::Expression(expr), tokens))
     } else {
-        Err(String::from("Expected semicolon"))
+        Err((String::from("Expected semicolon"), tokens))
+    }
+}
+
+pub enum Declaration {
+    Variable(String, Option<Expr>),
+    Statement(Stmt),
+}
+
+fn declaration(
+    mut tokens: Peekable<Iter<Token>>,
+) -> Result<(Declaration, Peekable<Iter<Token>>), (String, Peekable<Iter<Token>>)> {
+    match tokens.peek() {
+        Some(Token::KWD(Keyword::VAR, _)) => {
+            let _ = tokens.next();
+            let (declaration, tokens) = variable(tokens)?;
+            Ok((declaration, tokens))
+        }
+        _ => {
+            let (stmt, tokens) = statement(tokens)?;
+            Ok((Declaration::Statement(stmt), tokens))
+        }
+    }
+}
+
+fn variable(
+    mut tokens: Peekable<Iter<Token>>,
+) -> Result<(Declaration, Peekable<Iter<Token>>), (String, Peekable<Iter<Token>>)> {
+    if let Some(Token::IDEN(name, _)) = tokens.peek() {
+        let _ = tokens.next();
+        if let Some(Token::OPER(Operator::EQUAL, _)) = tokens.peek() {
+            let _ = tokens.next();
+            let (expr, mut tokens) = expression(tokens)?;
+            if let Some(Token::PUNC(Punctuation::SEMICOLON, _)) = tokens.peek() {
+                let _ = tokens.next();
+                Ok((
+                    Declaration::Variable(String::from(name), Some(expr)),
+                    tokens,
+                ))
+            } else {
+                Err((String::from("Expected semicolon"), tokens))
+            }
+        } else if let Some(Token::PUNC(Punctuation::SEMICOLON, _)) = tokens.peek() {
+            let _ = tokens.next();
+            Ok((Declaration::Variable(String::from(name), None), tokens))
+        } else {
+            Err((String::from("Expected semicolon"), tokens))
+        }
+    } else {
+        Err((String::from("Expected identifier"), tokens))
+    }
+}
+
+pub fn parse(tokens: Vec<Token>) -> Result<Vec<Declaration>, String> {
+    let mut declarations: Vec<Declaration> = vec![];
+    let mut tokens = tokens.iter().peekable();
+
+    while tokens.peek().is_some() {
+        let token = tokens.peek();
+        match token {
+            Some(Token::EOF(_)) | None => break,
+            _ => {
+                match declaration(tokens) {
+                    Ok((declaration, new_tokens)) => {
+                        declarations.push(declaration);
+                        tokens = new_tokens;
+                    }
+                    Err((_, mut new_tokens)) => {
+                        synchronize(&mut new_tokens);
+                        tokens = new_tokens;
+                    }
+                }
+            }
+        }
+    }
+    Ok(declarations)
+}
+
+fn synchronize(tokens: &mut Peekable<Iter<Token>>) {
+    while let Some(token) = tokens.peek() {
+        match token {
+            Token::PUNC(Punctuation::SEMICOLON, _) => break,
+            Token::KWD(Keyword::CLASS, _) => break,
+            Token::KWD(Keyword::FUN, _) => break,
+            Token::KWD(Keyword::VAR, _) => break,
+            Token::KWD(Keyword::FOR, _) => break,
+            Token::KWD(Keyword::IF, _) => break,
+            Token::KWD(Keyword::WHILE, _) => break,
+            Token::KWD(Keyword::PRINT, _) => break,
+            Token::KWD(Keyword::RETURN, _) => break,
+            Token::EOF(_) => break,
+            _ => (),
+        }
+        let _ = tokens.next();
     }
 }
