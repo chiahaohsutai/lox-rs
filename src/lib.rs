2e24daa2ae6ea312af7cc8917d06385b13393fb3
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2025-01-18 13:22:04 -0500

    added post tokenization error detection

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
 mod tokenizer;
 
-fn interpret<T: AsRef<str>>(program: T) -> Result<(), String> {
+fn print_errors(errors: Vec<String>) {
+    for error in errors {
+        eprintln!("{}", error);
+    }
+}
+
+pub fn interpret<T: AsRef<str>>(program: T) -> Result<(), String> {
     let tokens = tokenizer::tokenize(program.as_ref());
-    println!("{:?}", tokens);
+    let has_errors = tokens.iter().any(|t| t.is_err());
+    if has_errors {
+        let errors: Vec<_> = tokens.into_iter().filter_map(|t| t.err()).collect();
+        for error in errors {
+            eprintln!("{}", error);
+            std::process::exit(65)
+        };
+        
+    };
     Ok(())
 }
