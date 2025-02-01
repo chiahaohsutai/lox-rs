0a5b75a7d0b6ba9156274494aad330a36e8412c2
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2025-02-01 12:58:47 -0500

    new lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,103 @@
-mod evaluate;
-mod parser;
+mod expressions;
 mod tokenizer;
 
-use crate::evaluate::evaluate;
 use std::collections::HashMap;
-use std::vec;
+use std::fmt::Display;
 
-pub fn interpret<T: AsRef<str>>(program: T) {
-    let tokens = tokenizer::tokenize(program.as_ref());
-    let has_errors = tokens.iter().any(|t| t.is_err());
-    if has_errors {
-        let errors: Vec<_> = tokens.into_iter().filter_map(|t| t.err()).collect();
-        for error in errors {
-            eprintln!("{}", error);
-            std::process::exit(65)
+#[derive(Debug, Clone, PartialEq)]
+enum Object {
+    Function(String, usize, fn(Vec<Object>) -> Option<Object>),
+    Number(f64),
+    String(String),
+    Boolean(bool),
+}
+
+impl Display for Object {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Object::Function(name, _, _) => write!(f, "<fn {}>", name),
+            Object::Number(num) => write!(f, "{}", num),
+            Object::String(s) => write!(f, "{}", s),
+            Object::Boolean(b) => write!(f, "{}", b),
         }
-    } else {
-        let tokens: Vec<_> = tokens.into_iter().map(|t| t.unwrap()).collect();
-        let (statements, errors) = parser::parse(tokens);
-        if !errors.is_empty() {
-            for error in errors {
-                eprintln!("{}", error);
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct Enviorment {
+    parent: Option<Box<Enviorment>>,
+    values: HashMap<String, Object>,
+}
+
+impl Enviorment {
+    fn define(&mut self, key: String, value: Object) {
+        self.values.insert(key, value);
+    }
+
+    fn assign(&mut self, key: String, value: Object) -> Result<Object, String> {
+        if self.values.contains_key(&key) {
+            let prev = self.values.remove(&key);
+            self.values.insert(key, value);
+            Ok(prev.unwrap())
+        } else {
+            match &mut self.parent {
+                Some(parent) => parent.assign(key, value),
+                None => Err(format!("Undefined variable '{}'", key)),
             }
-            std::process::exit(65)
         }
-        let mut enviormnent = vec![HashMap::new()];
-        for statement in statements {
-            let result = evaluate(statement, &mut enviormnent);
-            if let Result::Err(e) = result {
-                eprintln!("{}", e);
-                std::process::exit(70)
+    }
+
+    fn get(&self, key: String) -> Result<Object, String> {
+        if self.values.contains_key(&key) {
+            Ok(self.values.get(&key).unwrap().clone())
+        } else {
+            match &self.parent {
+                Some(parent) => parent.get(key),
+                None => Err(format!("Undefined variable '{}'", key)),
             }
         }
-    };
+    }
+}
+
+impl Default for Enviorment {
+    fn default() -> Self {
+        Enviorment {
+            parent: None,
+            values: HashMap::new(),
+        }
+    }
+}
+
+impl From<Box<Enviorment>> for Enviorment {
+    fn from(env: Box<Enviorment>) -> Self {
+        Enviorment {
+            parent: Some(env),
+            values: HashMap::new(),
+        }
+    }
+}
+
+struct Interpreter {
+    env: Enviorment,
+}
+
+impl Default for Interpreter {
+    fn default() -> Self {
+        Interpreter {
+            env: Enviorment::default(),
+        }
+    }
+}
+
+impl Interpreter {
+    fn eval(&mut self, program: &str) -> Result<Object, String> {
+        // let tokens = tokenizer::tokenize(program);
+        // let mut statements = statements::parse(tokens);
+        // let mut result = Object::Boolean(false);
+        // for statement in statements.iter_mut() {
+        //     result = statement.eval(&mut self.env)?;
+        // }
+        // Ok(result)
+        todo!()
+    }
 }
